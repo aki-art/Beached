8f3e4ca8be54219dc4f48368696d1295f8a9ebec
Aki Senkinn <akitary.art@gmail.com>
2023-06-13 21:33:45 +0200

    fixed sim crash (Asquared is great)

--- source/Beached/Patches/WaterCubesPatch.cs
+++ source/Beached/Patches/WaterCubesPatch.cs
@@ -15,7 +15,7 @@ namespace Beached.Patches
 		[HarmonyPatch(typeof(WaterCubes), nameof(WaterCubes.Init))]
 		public class WaterCubes_Init_Patch
 		{
-			public static IEnumerable<CodeInstruction> Transpiler(ILGenerator generator, IEnumerable<CodeInstruction> orig)
+/*			public static IEnumerable<CodeInstruction> Transpiler(ILGenerator generator, IEnumerable<CodeInstruction> orig)
 			{
 				var codes = orig.ToList();
 
@@ -23,7 +23,9 @@ namespace Beached.Patches
 					.GetMethod("AddComponent", new Type[] { })
 					.MakeGenericMethod(typeof(MeshFilter));
 
-				var index = codes.FindIndex(ci => ci.Calls(m_AddComponent));
+				var m_layer_setter = typeof(GameObject).GetProperty("layer").GetSetMethod();
+
+				var index = codes.FindIndex(ci => ci.Calls(m_layer_setter));
 
 				if (index == -1)
 				{
@@ -31,7 +33,7 @@ namespace Beached.Patches
 					return codes;
 				}
 
-				var m_InjectedMethod = AccessTools.DeclaredMethod(typeof(WaterCubes_Init_Patch), nameof(CacheMesh));
+				var m_InjectedMethod = AccessTools.DeclaredMethod(typeof(WaterCubes_Init_Patch), nameof(GetLayer));
 
 				// inject right at the found index
 				codes.InsertRange(index, new[]
@@ -43,65 +45,43 @@ namespace Beached.Patches
 				return codes;
 			}
 
-			private static void CacheMesh(MeshFilter mesh)
+			private static int GetLayer(int layer)
 			{
-				WaterCubesPatch.mesh = mesh;
+				return layer | LayerMask.NameToLayer("Water");
 			}
-
+*/
 			public static void Postfix(WaterCubes __instance)
 			{
-				Log.Debug("postfix");
-				mesh ??= __instance.cubes.GetComponentInChildren<MeshFilter>();
+				var waterCubesMesh = __instance.transform.Find("WaterCubesMesh");
 
-				Log.Debug("1");
-				FUtility.FUI.Helper.ListChildren(__instance.cubes.transform);
-
-				Log.Debug("2");
-				if (mesh != null)
-				{
-					Log.Debug("3");
-					mesh.gameObject.SetLayerRecursively(LayerMask.NameToLayer("Water"));
-					Log.Debug("3b");
-				}
-				else
-					Log.Warning("WaterCubes mesh is null");
+				if (waterCubesMesh != null)
+					waterCubesMesh.gameObject.SetLayerRecursively(LayerMask.NameToLayer("Water") | waterCubesMesh.gameObject.layer);
 
 				// make the liquids a little more see-through
-				//__instance.material.SetFloat("_BlendScreen", 0.5f);
+				__instance.material.SetFloat("_BlendScreen", 0.5f);
 
-				Log.Debug("4");
 				var gameObject = new GameObject("Beached_WaterWobbleMesh");
-
-				Log.Debug("4b");
 
 				if (__instance.cubes == null)
 					Log.Warning("cubes is null");
 
 				gameObject.transform.parent = __instance.cubes.transform;
 
-				Log.Debug("5");
 				var meshFilter = gameObject.AddComponent<MeshFilter>();
 
 				var meshRenderer = gameObject.AddComponent<MeshRenderer>();
-				Log.Debug("6");
 				meshRenderer.sharedMaterial = ModAssets.Materials.liquidRefractionMat;
-				Log.Debug("7");
 				meshRenderer.shadowCastingMode = ShadowCastingMode.Off;
 				meshRenderer.receiveShadows = false;
 				meshRenderer.lightProbeUsage = LightProbeUsage.Off;
 				meshRenderer.reflectionProbeUsage = ReflectionProbeUsage.Off;
 
-				Log.Debug("8");
 				meshFilter.sharedMesh = __instance.CreateNewMesh();
-				Log.Debug("9");
 				meshRenderer.gameObject.layer = 0;
 				meshRenderer.gameObject.transform.parent = __instance.transform;
 				meshRenderer.gameObject.transform.position = new Vector3(0, 0, Grid.GetLayerZ(Grid.SceneLayer.Liquid));
 
-				Log.Debug("10");
 				gameObject.SetActive(true);
-
-				Log.Debug("postfix end");
 			}
 		}
 #endif
