4daffd5295be560c387916d368de1b206feb4f8c
Aki Senkinn <akitary.art@gmail.com>
2023-11-16 12:17:04 +0100

    Update to Packed Snacks

--- source/Beached/Patches/EdiblePatch.cs
+++ source/Beached/Patches/EdiblePatch.cs
@@ -6,9 +6,17 @@ namespace Beached.Patches
 {
 	public class EdiblePatch
 	{
-		[HarmonyPatch(typeof(Edible), nameof(Edible.AddOnConsumeEffects))]
 		public class Edible_AddOnConsumeEffects_Patch
 		{
+			// patched later, because his method calls a static property with a getter that references Db.Get(), which crashes if called too early
+			public static void Patch(Harmony harmony)
+			{
+				var targetMethod = AccessTools.Method("Edible:AddOnConsumeEffects", new[] { typeof(Worker) });
+				var postfix = AccessTools.Method(typeof(Edible_AddOnConsumeEffects_Patch), nameof(Postfix));
+
+				harmony.Patch(targetMethod, postfix: new HarmonyMethod(postfix));
+			}
+
 			public static void Postfix(Edible __instance, Worker worker)
 			{
 				if (worker == null)
