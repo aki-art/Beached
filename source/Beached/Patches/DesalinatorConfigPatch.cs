1b4328f5aad13c097228d5244a1b825599b0e02d
Aki Senkinn <akitary.art@gmail.com>
2024-10-08 00:25:02 +0200

    Desalinate murky brine

---
+++ source/Beached/Patches/DesalinatorConfigPatch.cs
@@ -0,0 +1,29 @@
+﻿using Beached.Content;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Beached.Patches
+{
+	public class DesalinatorConfigPatch
+	{
+		[HarmonyPatch(typeof(DesalinatorConfig), "ConfigureBuildingTemplate")]
+		public class DesalinatorConfig_ConfigureBuildingTemplate_Patch
+		{
+			public static void Postfix(GameObject go)
+			{
+				var murkyBrineToPollutedWater = go.AddComponent<ElementConverter>();
+
+				murkyBrineToPollutedWater.consumedElements =
+				[
+					new ElementConverter.ConsumedElement(Elements.murkyBrine.CreateTag(), 5f)
+				];
+
+				murkyBrineToPollutedWater.outputElements =
+				[
+					new ElementConverter.OutputElement(3.5f, SimHashes.DirtyWater, 0f, storeOutput: true),
+					new ElementConverter.OutputElement(1.5f, SimHashes.Salt, 0f, storeOutput: true)
+				];
+			}
+		}
+	}
+}
