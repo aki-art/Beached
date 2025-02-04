557093e1884b30090dca17ce14ae5745087c7483
Aki Senkinn <akitary.art@gmail.com>
2025-02-04 02:25:35 +0100

    moonlet api check

---
+++ source/Beached/Utils/MoonletMissingException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Beached.Utils
+{
+	public class MoonletMissingException : Exception
+	{
+		public MoonletMissingException() : base("Moonlet is a required API to run Beached. Please install and enable it.")
+		{
+		}
+	}
+}
