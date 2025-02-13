57d39955fd7e3b510ad6c7f9665863d44ceec88c
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-02-13 22:14:15 +0100

    MeteorSwarmVisualizer

--- source/Beached/Patches/ClusterMapScreenPatch.cs
+++ source/Beached/Patches/ClusterMapScreenPatch.cs
@@ -1,6 +1,9 @@
 ﻿using Beached.Content.Defs.StarmapEntities;
 using Beached.Content.Scripts.Entities;
+using Beached.Content.Scripts.StarmapEntities;
 using HarmonyLib;
+using System.Linq;
+using static LogicGateVisualizer;
 
 namespace Beached.Patches
 {
@@ -11,12 +14,25 @@ namespace Beached.Patches
 		{
 			public static void Postfix(ClusterMapScreen __instance)
 			{
+				foreach(var entityGroup in ClusterGrid.Instance.cellContents)
+				{
+					foreach(var entity in entityGroup.Value)
+					{
+						if(entity.TryGetComponent<SwarmVisualizer>(out var visualizer))
+						{
+							visualizer.SpawnVisualizers();
+							return;
+						}
+					}
+				}
+
 				foreach (var vis in __instance.m_gridEntityVis)
 				{
+					//note: this is only found if the MeteorSwarmVisualPOI is uncovered!
 					if (vis.Key.IsPrefabID(MeteorSwarmVisualPOIConfig.ID))
 					{
 						Log.Debug("ADDED ASTEROID BELT VIS");
-						vis.Value.gameObject.AddOrGet<AsteroidBeltVisualizer>();
+						//vis.Value.gameObject.AddOrGet<AsteroidBeltVisualizer>();
 					}
 				}
 			}
