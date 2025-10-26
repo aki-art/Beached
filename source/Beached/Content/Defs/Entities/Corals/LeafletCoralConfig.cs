83e7a187417964c5fbada3ed095d9da3b55228a5
Aki Senkinn <akitary.art@gmail.com>
2025-10-26 02:05:49 +0200

    some worldgen changes + assets

--- source/Beached/Content/Defs/Entities/Corals/LeafletCoralConfig.cs
+++ source/Beached/Content/Defs/Entities/Corals/LeafletCoralConfig.cs
@@ -1,5 +1,7 @@
-﻿/*using Beached.Content.Scripts.Entities;
-using TUNING;
+﻿using Beached.Content.DefBuilders;
+using Beached.Content.Scripts.Entities;
+using Beached.Content.Scripts.Entities.Plant;
+using System;
 using UnityEngine;
 
 namespace Beached.Content.Defs.Entities.Corals
@@ -8,92 +10,19 @@ namespace Beached.Content.Defs.Entities.Corals
 	{
 		public const string ID = "Beached_LeafletCoral";
 		public const string SEED_ID = "Beached_LeafletCoralSeed";
-		public const string BASE_TRAIT_ID = "Beached_LeafletCoral_Original";
-		public const string PREVIEW_ID = "Beached_LeafletCora_Preview";
-
 		public const float CONVERSION_RATE = 0.5f;
-		public const float SUCC_RATE = 0.01f;
+		public const float SUCC_RATE = 0.05f;
+		public const float RATE_MULT = 2f;
 
 		public GameObject CreatePrefab()
 		{
-			var prefab = EntityTemplates.CreatePlacedEntity(
-				ID,
-				STRINGS.CREATURES.SPECIES.BEACHED_LEAFLETCORAL.NAME,
-				STRINGS.CREATURES.SPECIES.BEACHED_LEAFLETCORAL.DESC,
-				10f,
-				Assets.GetAnim("beached_leaflet_coral_kanim"),
-				"idle_grown",
-				Grid.SceneLayer.BuildingBack,
-				1,
-				1,
-				DECOR.BONUS.TIER0,
-				additionalTags:
-				[
-					BTags.aquaticPlant,
-					BTags.coral
-				]);
-
-			EntityTemplates.ExtendEntityToBasicPlant(
-				prefab,
-				MiscUtil.CelsiusToKelvin(0f),
-				MiscUtil.CelsiusToKelvin(5f),
-				MiscUtil.CelsiusToKelvin(42f),
-				MiscUtil.CelsiusToKelvin(50f),
-				[
-					SimHashes.Water,
-					SimHashes.SaltWater,
-					SimHashes.Brine,
-					SimHashes.DirtyWater,
-					Elements.murkyBrine
-				],
-				false,
-				0f,
-				0.15f,
-				null,
-				false,
-				true,
-				false,
-				true,
-				2400f,
-				0f,
-				7400f,
-				BASE_TRAIT_ID,
-				STRINGS.CREATURES.SPECIES.BEACHED_LEAFLETCORAL.NAME);
-
-			prefab.AddOrGet<SubmersionMonitor>();
-			prefab.GetComponent<UprootedMonitor>();
-			prefab.GetComponent<FoundationMonitor>();
-			prefab.AddOrGet<LoopingSounds>();
-
-			var seed = EntityTemplates.CreateAndRegisterSeedForPlant(
-				prefab,
-				SeedProducer.ProductionType.DigOnly,
-				SEED_ID,
-				STRINGS.CREATURES.SPECIES.SEEDS.BEACHED_LEAFLETCORAL.NAME,
-				STRINGS.CREATURES.SPECIES.SEEDS.BEACHED_LEAFLETCORAL.DESC,
-				Assets.GetAnim("beached_leaflet_coral_frag_kanim"),
-				additionalTags:
-				[
-					GameTags.WaterSeed,
-					BTags.coralFrag,
-					BTags.smallAquariumSeed
-				],
-				sortOrder: 3,
-				domesticatedDescription: STRINGS.CREATURES.SPECIES.BEACHED_LEAFLETCORAL.DOMESTICATEDDESC,
-				width: 0.33f,
-				height: 0.33f,
-				ignoreDefaultSeedTag: true);
-
-			var preview = EntityTemplates.CreateAndRegisterPreviewForPlant(
-				seed,
-				PREVIEW_ID,
-				Assets.GetAnim("beached_leaflet_coral_kanim"),
-				"place",
-				1,
-				1);
-
-			preview.GetComponent<EntityPreview>().objectLayer = ObjectLayer.Plants;
-			preview.GetComponent<OccupyArea>().objectLayers = [ObjectLayer.Plants];
+			var prefab = new CoralBuilder(ID, "beached_leaflet_coral_kanim")
+				.Harvestable()
+				.Age(6 * CONSTS.CYCLE_LENGTH)
+				.SafeTemperaturesCelsius(0, 5, 42, 50)
+				.InitialAnim("idle_grown")
+				.Frag("beached_leaflet_coral_frag_kanim", additionalTags: [GameTags.WaterSeed])
+				.Build().entityPrefab;
 
 			var storage = prefab.AddOrGet<Storage>();
 			storage.showInUI = true;
@@ -101,7 +30,7 @@ namespace Beached.Content.Defs.Entities.Corals
 
 			var passiveElementConsumer = prefab.AddOrGet<PassiveElementConsumer>();
 			passiveElementConsumer.elementToConsume = SimHashes.Water;
-			passiveElementConsumer.consumptionRate = 0.2f;
+			passiveElementConsumer.consumptionRate = 0.05f * RATE_MULT;
 			passiveElementConsumer.capacityKG = 10f;
 			passiveElementConsumer.consumptionRadius = 3;
 			passiveElementConsumer.showInStatusPanel = true;
@@ -112,26 +41,23 @@ namespace Beached.Content.Defs.Entities.Corals
 
 			var coral = prefab.AddComponent<Coral>();
 			coral.emitTag = GameTags.Gas;
-			coral.emitMass = 0.05f;
+			coral.emitMass = 0.2f;
 			coral.initialVelocity = new Vector2f(0, 1);
-			coral.consumptionRate = 0.2f;
+			coral.consumptionRate = 0.05f * RATE_MULT;
+			coral.spawnFX = SpawnFXHashes.OxygenEmissionBubbles;
+
+			prefab.AddComponent<LeafletCoral>();
 
 			CoralTemplate.AddSimpleConverter(prefab, SimHashes.Water, SUCC_RATE, SimHashes.Oxygen, SUCC_RATE * CONVERSION_RATE);
 
 			return prefab;
 		}
 
+		[Obsolete]
 		public string[] GetDlcIds() => null;
 
 		public void OnPrefabInit(GameObject inst) { }
 
-		public void OnSpawn(GameObject inst)
-		{
-			if (inst.TryGetComponent(out ElementConsumer consumer))
-			{
-				consumer.EnableConsumption(true);
-			}
-		}
+		public void OnSpawn(GameObject inst) { }
 	}
 }
-*/
