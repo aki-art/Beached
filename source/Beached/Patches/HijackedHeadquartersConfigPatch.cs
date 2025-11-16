d36a501cb189284b1f0a43d31575060722ca4699
Aki Senkinn <akitary.art@gmail.com>
2025-11-16 11:27:17 +0100

    Printerceptor tweaks

---
+++ source/Beached/Patches/HijackedHeadquartersConfigPatch.cs
@@ -0,0 +1,20 @@
+﻿using Beached.Content.Scripts;
+using HarmonyLib;
+
+namespace Beached.Patches
+{
+	public class HijackedHeadquartersConfigPatch
+	{
+		[HarmonyPatch(typeof(HijackedHeadquartersConfig), "GetDataBankCost")]
+		public class HijackedHeadquartersConfig_GetDataBankCost_Patch
+		{
+			public static void Postfix(Tag printableTag, ref int __result)
+			{
+				if (Beached_WorldLoader.Instance.PrinterceptorDisabled(printableTag))
+				{
+					__result = int.MaxValue;
+				}
+			}
+		}
+	}
+}
---
+++ source/Beached/Patches/PrinterceptorScreenPatch.cs
@@ -0,0 +1,44 @@
+﻿using Beached.Content.Scripts;
+using HarmonyLib;
+using UnityEngine.UI;
+
+namespace Beached.Patches
+{
+	internal class PrinterceptorScreenPatch
+	{
+
+		[HarmonyPatch(typeof(PrinterceptorScreen), "SpawnOptionButton")]
+		public class PrinterceptorScreen_SpawnOptionButton_Patch
+		{
+			public static void Postfix(PrinterceptorScreen __instance, Tag id)
+			{
+				if (Beached_WorldLoader.Instance.PrinterceptorDisabled(id))
+				{
+					if (__instance.optionButtons.TryGetValue(id, out var button))
+					{
+						if (button.TryGetComponent(out HierarchyReferences refs))
+						{
+							if (refs.TryGetReference<Image>("FGIcon", out var icon))
+								icon.sprite = Assets.GetSprite("beached_glitched_egg_1");
+						}
+					}
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(PrinterceptorScreen), "SelectEntity")]
+		public class PrinterceptorScreen_SelectEntity_Patch
+		{
+			public static void Postfix(PrinterceptorScreen __instance, Tag id)
+			{
+				if (Beached_WorldLoader.Instance.PrinterceptorDisabled(id))
+				{
+					// TODO: STRING
+					__instance.selectedEffectsText.text = "<smallcaps><color=#b83846>C<b>ORRUPTED</color></b> D<b><i>A</i></b>TA▯▯▯. Cann▯t <size=140%>pri</size>nt this this this item▯▯▯▯▯.</smallcaps>\n\n" +
+						"<i>This data has been compromised. Finding a Hatch by other means first should allow my Duplicants to restore the missing information.</i>";
+
+				}
+			}
+		}
+	}
+}
